f438d120d0e185abde6c89b78072a681975bb9ec
Hitalo Emanoel <hitalo.emanoel@gmail.com>
2018-11-01 18:38:39 -0300

    Hello world with class and methods call

---
+++ MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello_c_2
+{
+
+    class MyClass
+    {
+        private int age = 27;
+
+        public MyClass(int age) {
+            this.age = age;
+        }
+
+        public int GetAge() {
+            return age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using hello_c_2;
+
+namespace hello_c_
+{
+
+    class AnotherClass 
+    {
+        private string name = "my name";
+
+        public AnotherClass(string name) {
+            this.name = name;
+        }
+
+        public string GetName() {
+            return name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyClass myClass = new MyClass(26);
+            Console.WriteLine(myClass.GetAge() - 1);   
+
+            AnotherClass anotherClass = new AnotherClass("another name");
+            Console.WriteLine(anotherClass.GetName());   
+        }
+    }
+}
